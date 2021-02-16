84733faf566a34598b3d8f157c04beb30a67142f
piotr.knap <piotr.knap@webchili.pl>
2021-02-16 21:07:22 +0100

    Style and Quality Fixes

--- src/Extension/BasicSecurity.php
+++ src/Extension/BasicSecurity.php
@@ -12,7 +12,29 @@ class BasicSecurity implements Extension
 {
     public function transformCode(string $code): string
     {
-        $notAllowedWords = ['$this', 'for', 'while', 'do', 'function', 'class', 'new', 'namespace', 'use', 'define', 'const'];
+        $notAllowedWords = [
+            '$this',
+            // loops without foreach
+            'for',
+            'while',
+            'do',
+            // definitions
+            'function',
+            'class',
+            'new',
+            'namespace',
+            'use',
+            'define',
+            'const',
+            // functions
+            'echo',
+            'eval',
+            'print',
+            'printf',
+            'printr',
+            'var_dump',
+            'var_export',
+        ];
         foreach ($notAllowedWords as $notAllowedWord) {
             if (1 === preg_match('/(^|\W)'.str_replace(['$'], ['\$'], $notAllowedWord).'($|\W)/', $code)) {
                 throw new ParseError('Using '.$notAllowedWord.' is not allowed');
